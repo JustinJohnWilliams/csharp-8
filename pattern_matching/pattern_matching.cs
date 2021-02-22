de12a85d320c981ff1be722e396995e390e54ec8
Justin Williams <justinjohnwilliams@gmail.com>
2021-02-22 09:57:43 -0600

    1c. Pattern Matching - Refactor

--- pattern_matching/pattern_matching.cs
+++ pattern_matching/pattern_matching.cs
@@ -18,65 +18,8 @@ public static class CardDescriber
 
         return sb.ToString();
     }
-    public static string DescribeWithSwitchExpression(CardValue value, CardSuit suit)
-    {
-        var sb = new StringBuilder();
 
-        sb.Append(DescribeWithSwitchExpression(value));
-        sb.Append(" of ");
-        sb.Append(DescribeWithSwitchExpression(suit));
-
-        return sb.ToString();
-    }
-
-    private static string Describe(CardValue value)
-    {
-        var result = "";
-
-        switch(value)
-        {
-            case CardValue.Ace:
-            case CardValue.Jack:
-            case CardValue.Queen:
-            case CardValue.King:
-                result = value.FirstToUpper();
-                break;
-            case CardValue.Two:
-                result = "2";
-                break;
-            case CardValue.Three:
-                result = "3";
-                break;
-            case CardValue.Four:
-                result = "4";
-                break;
-            case CardValue.Five:
-                result = "5";
-                break;
-            case CardValue.Six:
-                result = "6";
-                break;
-            case CardValue.Seven:
-                result = "7";
-                break;
-            case CardValue.Eight:
-                result = "8";
-                break;
-            case CardValue.Nine:
-                result = "9";
-                break;
-            case CardValue.Ten:
-                result = "10";
-                break;
-            default:
-                result = "?";
-                break;
-        }
-
-        return result;
-    }
-
-    private static string DescribeWithSwitchExpression(CardValue value) =>
+    private static string Describe(CardValue value) =>
         value switch
         {
             var name when name == CardValue.Ace || Game.FaceCards.Contains(name)
@@ -93,27 +36,7 @@ public static class CardDescriber
             _               => "?"
         };
 
-    private static string Describe(CardSuit suit)
-    {
-        var result = "";
-
-        switch(suit)
-        {
-            case CardSuit.Clubs:
-            case CardSuit.Diamonds:
-            case CardSuit.Hearts:
-            case CardSuit.Spades:
-                result = suit.ToString().ToUpper();
-                break;
-            default:
-                result = "??";
-                break;
-        }
-
-        return result;
-    }
-
-    private static string DescribeWithSwitchExpression(CardSuit suit) =>
+    private static string Describe(CardSuit suit) =>
         suit switch
         {
             var name when (name == CardSuit.Clubs
